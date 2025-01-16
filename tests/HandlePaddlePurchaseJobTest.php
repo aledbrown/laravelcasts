f892375778160b56d47ba8eaf52c077fe034f6f9
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-01-16 15:57:40 +0000

    Create HandlePaddlePurchaseJobTest.php

---
+++ tests/HandlePaddlePurchaseJobTest.php
@@ -0,0 +1,77 @@
+<?php
+
+use App\Jobs\HandlePaddlePurchaseJob;
+use App\Mail\NewPurchaseMail;
+use App\Models\Course;
+use App\Models\PurchasedCourse;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\WebhookClient\Models\WebhookCall;
+
+uses(RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->dummyWebhookCall = WebhookCall::create([
+        'name' => 'default',
+        'url' => 'some-url',
+        'payload' => [
+            'email' => 'test@test.at',
+            'name' => 'Test User',
+            'p_product_id' => '34779',
+        ],
+    ]);
+});
+
+it('stores paddle purchase', function () {
+    // Act
+    Mail::fake();
+    $this->assertDatabaseCount(User::class, 0);
+    $this->assertDatabaseCount(PurchasedCourse::class, 0);
+
+    // Arrange
+    $course = Course::factory()->released()->create(['paddle_product_id' => '34779']);
+
+    // Act
+    (new HandlePaddlePurchaseJob($this->dummyWebhookCall))->handle();
+
+    // Assert
+    $this->assertDatabaseHas(User::class, [
+        'email' => $this->dummyWebhookCall->payload['email'],
+        'name' => $this->dummyWebhookCall->payload['name'],
+    ]);
+
+    $user = User::where('email', $this->dummyWebhookCall->payload['email'])->first();
+    $this->assertDatabaseHas(PurchasedCourse::class, [
+        'user_id' => $user->id,
+        'course_id' => $course->id,
+    ]);
+});
+
+it('stores paddle purchase with given user', function () {
+    // Arrange
+    Mail::fake();
+    $course = Course::factory()->create(['paddle_product_id' => '34779']);
+    $user = User::factory()->create(['email' => 'test@test.at']);
+
+    // Act
+    (new HandlePaddlePurchaseJob($this->dummyWebhookCall))->handle();
+
+    // Assert
+    $this->assertDatabaseCount(User::class, 1);
+    $this->assertDatabaseHas(PurchasedCourse::class, [
+        'user_id' => $user->id,
+        'course_id' => $course->id,
+    ]);
+});
+
+it('sends out purchase email', function () {
+    // Arrange
+    Mail::fake();
+    Course::factory()->create(['paddle_product_id' => '34779']);
+
+    // Act
+    (new HandlePaddlePurchaseJob($this->dummyWebhookCall))->handle();
+
+    // Assert
+    Mail::assertSent(NewPurchaseMail::class);
+});
